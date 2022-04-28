037bea826dcf1196528351765d2feb4769d65b59
marieooq <otakimarie@gmail.com>
2022-04-27 18:39:27 -0700

    create env variables

--- server/index.js
+++ server/index.js
@@ -1,5 +1,14 @@
 const express = require("express");
 const app = express();
+const mysql = require("mysql");
+require("dotenv").config();
+
+const db = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: "crud",
+});
 
 app.get("/", (req, res) => {
   res.send("hello world");
