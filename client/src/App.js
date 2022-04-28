0a09902b335db397c3eaa66f0ec987d6b2aa11b6
marieooq <otakimarie@gmail.com>
2022-04-27 19:18:15 -0700

    enables to send request

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,49 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./App.css";
 
 function App() {
+  const [movieName, setMovieName] = useState("");
+  const [review, setReview] = useState("");
+
+  const submitReview = () => {
+    axios
+      .post("http://localhost:3001/api/insert", {
+        movieName: movieName,
+        movieReview: review,
+      })
+      .then(() => {
+        alert("successfully insert");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <div className="App">
       <h1>CRUD Application</h1>
 
       <div className="form">
         <label>Movie Name:</label>
-        <input type="text" name="movieName" />
+        <input
+          type="text"
+          name="movieName"
+          onChange={(e) => {
+            setMovieName(e.target.value);
+          }}
+        />
         <label>Review:</label>
-        <input type="text" name="review" />
+        <input
+          type="text"
+          name="review"
+          onChange={(e) => {
+            setReview(e.target.value);
+          }}
+        />
       </div>
 
-      <button>submit</button>
+      <button onClick={submitReview}>submit</button>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const mysql = require("mysql");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 require("dotenv").config();
 
 const db = mysql.createPool({
@@ -10,8 +12,20 @@ const db = mysql.createPool({
   database: "crud",
 });
 
-app.get("/", (req, res) => {
-  res.send("hello world");
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post("/api/insert", (req, res) => {
+  const movieName = req.body.movieName;
+  const movieReview = req.body.movieReview;
+
+  const sqlInsert =
+    "INSERT INTO movie_reviews (movieName, movieReview) VALUES (?, ?);";
+
+  db.query(sqlInsert, [movieName, movieReview], (err, result) => {
+    console.log(result);
+  });
 });
 
 app.listen(3001, () => {
